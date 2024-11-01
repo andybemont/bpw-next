80b9d8c6af5efbff941a142c3d28b606b0c67762
Andy Bemont <andy.bemont@calero.com>
2024-11-01 15:42:40 -0400

    meta

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,11 @@ import { Metadata } from "next";
 import OverviewContent from "./ui/overview-content/overview-content";
 import SitePage from "./ui/shared/site-page";
 import { namedPictures } from "./lib/portfolio";
-import Page from "./contact/page";
 
 export const metadata: Metadata = {
   title:
     "Rochester Wedding Photography | Packages and Availability | Bemont Photo",
   description: "Great wedding photography team for all of Upstate New York.",
-  openGraph: {
-    siteName: "Rochester Wedding Photography by Bemont Photo",
-    title: "Rochester Wedding Photography by Bemont Photo",
-    description:
-      "Wedding Photography for Rochester, Buffalo, and the Finger Lakes",
-    url: "https://www.bemontphoto.com",
-    images: {
-      url: "/portfolio/BPBO5-1140.jpg",
-      alt: "Wedding Photography by Bemont Photo",
-    },
-  },
 };
 
 export default function Page() {
