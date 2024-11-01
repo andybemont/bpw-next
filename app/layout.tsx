8ab1caf33f24696acb37dad9ec74e645d658c0b4
Andy Bemont <andy.bemont@calero.com>
2024-11-01 15:41:34 -0400

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,21 @@
 import "@/app/ui/global.css";
 import { titleText } from "@/app/ui/fonts";
 import Header from "./ui/header/header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  openGraph: {
+    siteName: "Rochester Wedding Photography by Bemont Photo",
+    title: "Rochester Wedding Photography by Bemont Photo",
+    description:
+      "Wedding Photography for Rochester, Buffalo, and the Finger Lakes",
+    url: "https://www.bemontphoto.com",
+    images: {
+      url: "/portfolio/BPBO5-1140.jpg",
+      alt: "Wedding Photography by Bemont Photo",
+    },
+  },
+};
 
 export default function RootLayout({
   children,
