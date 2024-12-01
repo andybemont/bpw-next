199b875adb9e01b6c60a09cd099f4b1e57a36a59
Andy Bemont <andy.bemont@calero.com>
2024-12-01 11:27:26 -0500

    redirects for old pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,283 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+};
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/blank-2/erica-terry-a-downtown-rochester-wedding",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/2018/09/25/amanda-and-jake",
+        destination: "/",
+        permanent: true,
+      },
+      { source: "/events", destination: "/", permanent: true },
+      {
+        source: "/blank-2/wedding-photography-amanda-and-isaac",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/elise-maraj-a-wedding-at-greek-peak",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/senior-pictures-isabelle-at-the-park",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/senior-pictures-andrew",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photography-christine-and-ryan",
+        destination: "/",
+        permanent: true,
+      },
+      { source: "/blog/categories/senior", destination: "/", permanent: true },
+      {
+        source: "/blank-2/alex-dan-a-wedding-at-woodcliff",
+        destination: "/",
+        permanent: true,
+      },
+      { source: "/blog/page/4", destination: "/", permanent: true },
+      {
+        source: "/blank-2/wedding-photography-alyssa-and-anthony",
+        destination: "/",
+        permanent: true,
+      },
+      { source: "/wedding-photography-faq", destination: "/", permanent: true },
+      { source: "/blog", destination: "/", permanent: true },
+      {
+        source: "/blank-2/senior-pictures-senior-pictures-at-village-gate",
+        destination: "/",
+        permanent: true,
+      },
+      { source: "/portraits", destination: "/", permanent: true },
+      { source: "/pets", destination: "/", permanent: true },
+      {
+        source: "/blank-2/jess-jordan-a-wedding-at-wadsworth-homestead",
+        destination: "/",
+        permanent: true,
+      },
+      { source: "/family", destination: "/", permanent: true },
+      { source: "/copy-of-weddings", destination: "/", permanent: true },
+      {
+        source: "/blank-2/senior-pictures-julia-downtown",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/annie-hoa-a-wedding-at-del-lago",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/lena-uros-engagement-session-at-highland-park",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/2018/08/10/gretchen-and-shawn",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/pet-photography-pet-portraits-for-smudge",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/event-photography-tereasa",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/engagement-session-derek-allison",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/jessica-juan-a-wedding-at-casa-larga-vineyard",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photography-heather-and-tim",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photography-rachel-and-blaise",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/engagement-photos-engagement-at-genesee-valley-park",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/family-photography-the-longos-by-the-lake",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/event-photography-the-wedding-of-shep-and-zoe",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/family-photography-family-pictures-at-tinker",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source:
+          "/blank-2/engagement-pictures-a-couple-and-a-dog-at-genesee-valley-park",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photography-dan-and-marisa",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photography-jay-and-lindsay-get-married",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/family-photography-lucas",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source:
+          "/blank-2/senior-pictures-senior-pictures-at-tinker-nature-park",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/family-photography-family-photos-at-akron-acres",
+        destination: "/",
+        permanent: true,
+      },
+      { source: "/engagements", destination: "/", permanent: true },
+      {
+        source: "/blank-2/family-photography-heather-skivington",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/family-photography-family-photos-at-akron-falls",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/pet-photography-gracie-and-hugo",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photography-tori-and-dakota",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/family-photography-christmas-pictures-at-home",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/jenn-ryan-engagement-session-at-sunken-gardens",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/pet-photography-boris",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/katie-cory-a-wedding-at-the-otesaga",
+        destination: "/",
+        permanent: true,
+      },
+      { source: "/blog/page/11", destination: "/", permanent: true },
+      {
+        source: "/blank-2/pet-photography-rileys-big-shoot",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photography-karen-and-joe",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/andrea-eric-a-park-wedding-in-chili-ny",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photography-salome-and-matthew",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/kaitlyn-ken-engagement-session-at-nazareth-college",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/the-zollars-maternity-session-at-turning-point-park",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photography-jess-and-jeremy",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photography-christy-and-ryan",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photography-krystina-and-tristan",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source:
+          "/blank-2/wedding-photography-the-wedding-of-jenna-and-dominick",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photograph-yalesha-and-dan",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/blank-2/wedding-photography-mary-and-jeff",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
